2f9e66c44a4433933f662c34b5fc83ef08a6fabc
MyakalaVignesh <myakalavignesh01@gmail.com>
2026-02-22 18:45:08 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.js";
+import chatRoutes from "./routes/chat.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+
+app.get("/", (req, res) => {
+  res.send("ConfidantAI API Running");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
